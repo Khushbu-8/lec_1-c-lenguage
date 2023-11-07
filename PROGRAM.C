239dc990a647cceace046cc9356d049ee94ca9b3
khushbu-8 <khushbuzalavadiya8@gmail.com>
2023-11-07 15:32:49 +0530

    lec-1

---
+++ PROGRAM.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+clrscr();
+	printf("My name is khushbu zalavadiya\n");
+	printf("I am  21  year old\n");
+	printf("I am in  fullstack development\n");
+	printf("My GRID is 5679\n");
+	printf("I have completed my graduation from Amreli\n");
+	printf("i live in surat");
+
+getch();
+}
